042a727f901090123e20936a7938f79b78afde32
Ahmed Tawfik <beach_alex2009@yahoo.com>
2023-11-06 07:24:33 +0200

    update 6-11-2023

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -2,9 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\branch;
 use Illuminate\Http\Request;
 
 class BranchController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        //
+        $branches = Branch::query();
+        if ($request->has('search')) {
+            $branches->where('name', 'like', '%' . $request->search . '%');
+            $branches->orWhereHas('company', function ($q) use ($request) {
+                $q->where('name', 'like', '%' . $request->search . '%');
+            });
+        }
+
+        return view('branches.index', ["branches" => $branches->orderBy('created_at', 'desc')->paginate(20)]);
+        // return view('branches.index', ["branches" => $branches->orderBy('created_at', 'desc')->simplePaginate(20)]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BranchController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -29,19 +30,19 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Route::prefix('companies')->middleware('age')->name('companies.')->group(function () {
-        Route::prefix('companies')->name('companies.')->group(function () {
-            Route::get('', [CompanyController::class, 'index'])->name('index');
-            Route::get('create', [CompanyController::class, 'create'])->name('create'); //->middleware('age');
-            Route::post('store', [CompanyController::class, 'store'])->name('store');
+    Route::prefix('companies')->name('companies.')->group(function () {
+        Route::get('', [CompanyController::class, 'index'])->name('index');
+        Route::get('create', [CompanyController::class, 'create'])->name('create'); //->middleware('age');
+        Route::post('store', [CompanyController::class, 'store'])->name('store');
 
-            Route::delete('delete/{id}', [CompanyController::class, 'delete'])->name('delete');
+        Route::delete('delete/{id}', [CompanyController::class, 'delete'])->name('delete');
 
-            Route::get('edit/{id}', [CompanyController::class, 'edit'])->name('edit');
+        Route::get('edit/{id}', [CompanyController::class, 'edit'])->name('edit');
 
-            // Route::put('update/{id}', [CompanyController::class, 'edit'])->name('edit');
-            Route::patch('update/{id}', [CompanyController::class, 'update'])->name('update');
-        });
-
+        // Route::put('update/{id}', [CompanyController::class, 'edit'])->name('edit');
+        Route::patch('update/{id}', [CompanyController::class, 'update'])->name('update');
+        Route::resource('branches', BranchController::class)->except('show');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
